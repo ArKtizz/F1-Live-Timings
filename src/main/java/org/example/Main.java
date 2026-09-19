aaf4e434081832808658df4e6ac99c1a45da0733
arktizz <arktizzyt@gmail.com>
2026-09-19 15:24:15 +0200

    First declaration of Track and Driver + TimeFormatter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import org.example.model.Driver;
+import org.example.model.Team;
+import org.example.model.Track;
+import org.example.util.TimeFormatter;
+
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("test");
+        Driver verstappen = new Driver(3, "VER", Team.REDBULL);
+
+        System.out.println("MAX VERSTAPPEN\nTeam: "+verstappen.assignedTeam()+"\nShort name: "+verstappen.shortName()+"\nModifier: "+verstappen.assignedTeam().getTimeModifier());
+
+        Track suzuka = new Track("Suzuka", Duration.ofMillis(31500), Duration.ofMillis(40200), Duration.ofMillis(18100));
+
+        System.out.println("SUZUKA\n"+"First sector: "+suzuka.firstSector()+"\nSecond sector: "+suzuka.secondSector()+"\nThirdSector: "+suzuka.thirdSector());
+
+        System.out.println(TimeFormatter.format(suzuka.firstSector()));
 
     }
 }
---
+++ src/main/java/org/example/util/TimeFormatter.java
@@ -0,0 +1,21 @@
+package org.example.util;
+
+import org.example.model.Track;
+
+import java.time.Duration;
+
+public class TimeFormatter {
+    public static String format(Duration duration) {
+        int minutes = duration.toMinutesPart();
+        int seconds = duration.toSecondsPart();
+        int miliSeconds = duration.toMillisPart();
+
+        String formattedText = String.format("%d:%02d:%03d", minutes, seconds, miliSeconds);
+
+        return formattedText;
+    }
+
+    public static String FullLapTime(Track track) {
+        return "";
+    }
+}
